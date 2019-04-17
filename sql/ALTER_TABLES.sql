b551d77df052025ea1fe3c7639ba44fe0e9f9bf6
Wang <fdcdelabahan@gmail.com>
2019-04-17 15:58:30 +0800

    Add alters

---
+++ sql/ALTER_TABLES.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `order_details` ADD `description_request` TEXT NULL AFTER `sub_total`, ADD `status` ENUM('pending','success','falied') NOT NULL DEFAULT 'pending' AFTER `description_request`;
+ALTER TABLE `orders` ADD `resto_id` INT(11) NOT NULL AFTER `customer_id`, ADD `order_status` ENUM('pending','success','failed') NOT NULL DEFAULT 'pending' AFTER `resto_id`;
